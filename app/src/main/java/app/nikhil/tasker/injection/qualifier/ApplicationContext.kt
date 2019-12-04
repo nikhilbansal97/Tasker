4535772b8432c3ce2912c70d6f953b72b4aac866
Nikhil <nikhil97.nb@gmail.com>
2019-12-04 19:53:21 +0530

    Added missing qualifier for the ApplicationContext

--- app/src/main/java/app/nikhil/tasker/injection/qualifier/ApplicationContext.kt
+++ app/src/main/java/app/nikhil/tasker/injection/qualifier/ApplicationContext.kt
@@ -1,6 +1,8 @@
 package app.nikhil.tasker.injection.qualifier
 
+import javax.inject.Qualifier
 import kotlin.annotation.AnnotationRetention.RUNTIME
 
+@Qualifier
 @Retention(RUNTIME)
 annotation class ApplicationContext
